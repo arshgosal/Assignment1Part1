1e59be1831986184052b137ac770aa48609f82d3
arsh.gosal <deeparsh.da@gmail.com>
2020-06-25 11:09:25 -0400

    first

---
+++ PersonalPortfolio/routes/index.js
@@ -0,0 +1,22 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Personal Portfolio', name: 'Arshdeepak Ram' });
+});
+
+router.get('/aboutme', function (req, res) {
+    res.render('aboutme', { title: 'Personal Portfolio', name: 'Arshdeepak Ram' });
+});
+
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Contact Information'});
+});
+
+router.get('/Projects', function (req, res) {
+    res.render('projects', { title: 'Projects'});
+});
+
+module.exports = router;
